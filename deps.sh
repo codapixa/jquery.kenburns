20774097d9e138b720c3eb8dd290ac2be8eaabfb
JBlond <leet31337@web.de>
2015-09-01 16:44:36 +0200

    add grunt jshint

---
+++ deps.sh
@@ -0,0 +1,1 @@
+npm install grunt grunt-cli grunt-contrib-uglify grunt-contrib-jshint
---
+++ gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function (grunt) {
+	// Do grunt-related things in here
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+			},
+			build: {
+				src: 'src/jquery.kenburns.js',
+				dest:'src/jquery.kenburns.min.js'
+			}
+		},
+		jshint: {
+			options: {
+				curly: true,
+				eqeqeq: true,
+				eqnull: true,
+				browser: true,
+				globals: {
+					jQuery: true
+				},
+				devel: true
+			},
+			uses_defaults: 'src/jquery.kenburns.js',
+			with_overrides: {
+				options: {
+					curly: false,
+					undef: true
+				},
+				files: {
+					src: ['src/jquery.kenburns.js']
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	
+	// Default task(s).
+	grunt.registerTask('default', 'jshint');
+};
